9b5b39a8a0ef84c3d582fdbe975e7d66d393a8f3
Adrian F. Pizarro <adfpizarro@gmail.com>
2020-12-08 15:45:29 -0700

    Add style to the log form, removed unnecesary files

--- app/models/log_group.rb
+++ app/models/log_group.rb
@@ -1,4 +1,6 @@
 class LogGroup < ApplicationRecord
   belongs_to :group
-  belongs_to :log, class_name: TimeLog.name 
+  belongs_to :log, class_name: TimeLog.name
+
+  default_scope order('created_at ASC')
 end
