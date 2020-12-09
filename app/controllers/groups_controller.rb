c1aeca1b7e2f748e5e2e27aa656335a3fe4d2b62
Adrian F. Pizarro <adfpizarro@gmail.com>
2020-12-08 17:15:33 -0700

    Add style and scopes to the groups show section

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,6 +10,7 @@ class GroupsController < ApplicationController
   # GET /groups/1
   # GET /groups/1.json
   def show
+    @logs=@group.time_logs.order('created_at DESC')
   end
 
   # GET /groups/new
