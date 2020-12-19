f8f78de11c5e0255a667f0c5d60bd5a343b4ce5e
Adrian F. Pizarro <adfpizarro@gmail.com>
2020-12-18 19:16:34 -0700

    Update default scope for db groups

--- app/models/log_group.rb
+++ app/models/log_group.rb
@@ -2,5 +2,5 @@ class LogGroup < ApplicationRecord
   belongs_to :group
   belongs_to :log, class_name: TimeLog.name
 
-  default_scope order('created_at ASC')
+  default_scope {order('created_at ASC')}
 end
