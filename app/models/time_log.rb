280e8bb942830fe65afe40e595202929a1f9e7d0
Adrian F. Pizarro <adfpizarro@gmail.com>
2021-03-12 17:37:42 -0700

    Add validator to time log value

--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -1,6 +1,6 @@
 class TimeLog < ApplicationRecord
   validates :name, presence: true
-  validates :minutes, presence: true, numericality: true
+  validates :minutes, presence: true, numericality: {greater_than_or_equal_to: 1}
   belongs_to :user, class_name: User.name
   has_and_belongs_to_many :groups, class_name: 'Group'
 end
