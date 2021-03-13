e9585138c033e44c40f660e7eee6a2b3696cd298
Adrian F. Pizarro <adfpizarro@gmail.com>
2021-03-12 19:14:02 -0700

    Add default scope to order timelogs in a group

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,9 @@ class Group < ApplicationRecord
   validates :creator_id, presence: true
   has_one_attached :image
   belongs_to :creator, class_name: User.name
-  has_and_belongs_to_many :time_logs, class_name: 'TimeLog'
+  has_and_belongs_to_many :time_logs, class_name: 'TimeLog', :order => 'created_at DESC'
 
   scope :ordered_by_name, -> { order(name: :asc) }
+
+
 end
